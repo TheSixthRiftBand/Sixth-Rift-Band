c7f6180c39490a37e93845e4afcf9ae7b4c2e860
TheSixthRiftBand <the.sxith.rift.99@gmail.com>
2025-11-01 14:16:50 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
+import AdminSubscribers from "@/pages/admin-subscribers";
 import TrackDetail from "@/pages/track-detail";
 import NotFound from "@/pages/not-found";
 
@@ -12,6 +13,7 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/track/:id" component={TrackDetail} />
+      <Route path="/admin/subscribers" component={AdminSubscribers} />
       <Route component={NotFound} />
     </Switch>
   );
