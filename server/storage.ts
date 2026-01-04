6748c1bf3f0bcd7bd72619349e1df6322c641656
TheSixthRiftBand <the.sxith.rift.99@gmail.com>
2026-01-04 15:30:38 +0530

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -1,5 +1,5 @@
 import { type User, type InsertUser, type Subscriber, type InsertSubscriber } from "@shared/schema";
-import { randomUUID } from "crypto";
+import { randomUUID } from "crypto"; 
 
 // modify the interface with any CRUD methods
 // you might need
